73368805728c6a75b1fe3293106c871f8d4550ec
AnushaG2201 <76487202+AnushaG2201@users.noreply.github.com>
2021-01-04 14:47:23 +0530

    Create PrimeFactors.java

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,12 @@
+public class PrimeFactors{
+public static void main(String args[])
+{
+public List<Integers> findPrimeFactorsOfANumber(int numberToBeTested)
+{
+List <Integer> result=new ArrayList<Integer>();
+if(numberToBeTested==1){
+return List;
+}
+}
+}
+}
